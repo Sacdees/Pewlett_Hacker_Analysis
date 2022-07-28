58cedcda0deb8f88c9203c855833f9b4538781ff
Sacdees <104862958+Sacdees@users.noreply.github.com>
2022-07-28 00:16:34 -0400

    update minus readme

--- queries/employee_database_challenge.sql
+++ queries/employee_database_challenge.sql
@@ -1,3 +1,5 @@
+----------------------------------------------DELIVERABLE 1-----------------------------------------
+
 SELECT em.emp_no,
 	em.first_name,
 	em.last_name,
@@ -5,54 +7,57 @@ SELECT em.emp_no,
 	ti.title,
 	ti.from_date,
 	ti.to_date
-INTO retro_titles
-FROM emp_info as em
+INTO retirement_titles
+FROM full_emp as em
 	INNER JOIN titles as ti
 		ON(em.emp_no = ti.emp_no)
+WHERE em.birth_date between '1952-01-01' and '1955-12-31'
 ORDER BY em.emp_no;	
 
-select * from retro_titles
-select * from unique_titles
-
-------------------------------  To check that to date is 9999-01-01
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
-rt.first_name,
-rt.last_name,
-rt.title,
-rt.to_date
---INTO unique_titles
-FROM retro_titles as rt
-WHERE rt.to_date between '9999-01-01' and '9999-01-01'
-ORDER BY rt.emp_no, rt.to_date DESC;
+SELECT * FROM retirement_titles
 ------------------- for challenge point-----unique_titles
+
 SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
 rt.first_name,
 rt.last_name,
 rt.title
 INTO unique_titles
-FROM retro_titles as rt
+FROM retirement_titles as rt
 WHERE rt.to_date between '9999-01-01' and '9999-01-01'
 ORDER BY rt.emp_no, rt.to_date DESC;
 
---------------------------challenge oint----------retiring_titles
+SELECT * FROM unique_titles
 
-select * from unique_titles
-select * from retiring_titles
-
-
+--------------------------challenge point----------retiring_titles
 
 SELECT Count (ut.title), ut.title
 INTO retiring_titles
 FROM unique_titles as ut
-group by ut.title;
+group by ut.title
+order by ut.count DESC;
 
+SELECT * FROM retiring_titles
 
+----------------------------------------------DELIVERABLE 2----------------------------------------
 
+SELECT DISTINCT ON (em.emp_no)em.emp_no,
+	em.first_name,
+	em.last_name,
+	em.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title	
+INTO mentorship_data
+FROM full_emp as em
+	INNER JOIN dept_emp as de
+		ON(em.emp_no = de.emp_no)
+	INNER JOIN titles as ti
+		ON(em.emp_no = ti.emp_no)
+WHERE de.to_date between '9999-01-01' and '9999-01-01'
+and em.birth_date between '1965-01-01' and '1965-12-31'
+ORDER BY em.emp_no;	
 
-
-
-
+SELECT * FROM mentorship_data
 
 
 
