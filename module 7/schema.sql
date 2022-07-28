81d9aabec291684e84963bb431b06ba665df4074
Sacdees <104862958+Sacdees@users.noreply.github.com>
2022-07-27 20:22:42 -0400

    updates

--- module 7/schema.sql
+++ module 7/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE employees (
-	emp_no INT NOT NULL,
+	 emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
      last_name VARCHAR NOT NULL,
@@ -20,7 +20,7 @@ drop TABLE dept_manager
 
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -29,30 +29,40 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
+
+
 CREATE TABLE salaries (
-  emp_no INT NOT NULL,
-  salary INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+   	emp_no INT NOT NULL,
+  	salary INT NOT NULL,
+  	from_date DATE NOT NULL,
+  	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   	PRIMARY KEY (emp_no)
 );
 
+drop TABLE titles
+
 CREATE TABLE titles (
-  emp_no INT NOT NULL,
-  title VARCHAR NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  PRIMARY KEY (emp_no, title, from_date)
+  	emp_no INT NOT NULL,
+  	title VARCHAR NOT NULL,
+  	from_date DATE NOT NULL,
+  	to_date DATE NOT NULL,
+FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
+  	PRIMARY KEY (emp_no, title, from_date)
 );
 
+drop TABLE dept_emp
+
 CREATE TABLE dept_emp (
-  dept_no INT NOT NULL,
-  emp_no INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  PRIMARY KEY (dept_no, emp_no)
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
 );
+
 
 SELECT * FROM departments
 SELECT * FROM employees
@@ -64,9 +74,3 @@ SELECT * FROM dept_emp
 SELECT first_name, last_name
 FROM employees
 WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
-
-
-
-
-
-
